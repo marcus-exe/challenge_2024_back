3ecee516affe5ef1c09ab68bb05bcf72956c550f
Marcus Sena <mens.eai20@uea.edu.br>
2024-09-21 12:28:28 -0400

    Add ReadService and Change JsonReader Version

--- src/main/java/fiap/com/example/challenge/controller/FileController.java
+++ src/main/java/fiap/com/example/challenge/controller/FileController.java
@@ -1,5 +1,6 @@
 package fiap.com.example.challenge.controller;
 
+import fiap.com.example.challenge.service.FileReadService;
 import fiap.com.example.challenge.service.FileUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,13 +12,15 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/files")
-public class FileUploadController {
+public class FileController {
 
     private final FileUploadService fileUploadService;
+    private final FileReadService fileReadService;
 
     @Autowired
-    public FileUploadController(FileUploadService fileUploadService) {
+    public FileController(FileUploadService fileUploadService, FileReadService fileReadService) {
         this.fileUploadService = fileUploadService;
+        this.fileReadService = fileReadService;
     }
 
     @PostMapping("/upload")
@@ -27,6 +30,18 @@ public class FileUploadController {
             return ResponseEntity.ok("File uploaded successfully: " + file.getOriginalFilename());
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to upload file: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/read")
+    public ResponseEntity<String> readFileContent(@RequestParam("filePath") String filePath) {
+        try {
+            String content = fileReadService.readFileContent(filePath);
+            return ResponseEntity.ok(content);
+        } catch (IOException e) {
+            return ResponseEntity.status(500).body("Error reading file: " + e.getMessage());
+        } catch (UnsupportedOperationException e) {
+            return ResponseEntity.status(400).body(e.getMessage());
         }
     }
 }
